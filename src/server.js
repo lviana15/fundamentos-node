431534d344babbf175cfe015fef251cf3e9ee21a
Lucas Viana <lucasviana0604@gmail.com>
2023-01-11 13:16:37 -0300

    Headers

--- src/server.js
+++ src/server.js
@@ -6,10 +6,17 @@ const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method == 'GET' && url == '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method == 'POST' && url == '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com'
+    })
     return res.end('Criação de usuários')
   }
 
